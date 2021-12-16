edafe167d8e3ffcef93d47dc405298e66d30a079
변경연 <kissob4905@gmail.com>
2021-12-16 20:13:22 +0900

    ..

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,14 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountRate = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        return member.getGrade() == Grade.VIP ? price * discountRate / 100 : 0;
+    }
+}
